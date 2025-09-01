aedc54b94d43bf309567d2584ebc38cd9eb17a03
Ana Laura Lobos <anhalaurha@gmail.com>
2025-08-31 22:34:05 -0300

    init

---
+++ practico1/src/main/java/ar/edu/unlpam/ing/practico1/Practico1Application.java
@@ -0,0 +1,13 @@
+package ar.edu.unlpam.ing.practico1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Practico1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Practico1Application.class, args);
+	}
+
+}
---
+++ practico1/src/test/java/ar/edu/unlpam/ing/practico1/Practico1ApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.edu.unlpam.ing.practico1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Practico1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
